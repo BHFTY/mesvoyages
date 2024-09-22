3b79c0533ec61340633f4d7f72b5ded725ee15f6
BHFTY <cohenrivka.2005@gmail.com>
2024-09-22 15:09:43 +0200

    phase 13: ORM(ajout d'une visite).

--- src/Controller/admin/AdminVoyagesController.php
+++ src/Controller/admin/AdminVoyagesController.php
@@ -53,6 +53,23 @@ public function __construct(VisiteRepository $repository) {
             'formvisite'  =>$formVisite->createView()
         ]);
     }
+    
+    #[Route('/admin/ajout', name: 'admin.voyage.ajout')]
+    public function ajout(Request $request): Response {
+        $visite = new Visite();
+        $formVisite = $this->createForm(VisiteType::class, $visite);
+        
+        $formVisite->handleRequest($request);
+        if($formVisite ->isSubmitted() && $formVisite->isValid()){
+            $this->repository->add($visite);
+            return $this->redirectToRoute('admin.voyages');
+        }
+        return $this->render("admin/admin.voyage.ajout.html.twig",[
+            'visite' => $visite,
+            'formvisite'  =>$formVisite->createView()
+        ]);
+    }
+    
 }
 
      
